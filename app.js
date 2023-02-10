da18fbd84c3cfee868a6e06dc208062a30911e3a
emanikor <87859876+emanikor@users.noreply.github.com>
2023-02-10 16:13:39 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const myForm =  document.querySelector('#form-lable');
 const messageBtn=  document.querySelector('#message-btn');
 const  emailInput=  document.querySelector('#email');
 const nameInput =  document.querySelector('#name');
-const msgField =  document.querySelector('.message');
+const msgField =  document.querySelector('#message');
 const msg=  document.querySelector('.msg');
 
 myForm.addEventListener('submit' , onSubmit);
@@ -26,15 +26,20 @@ function onSubmit(e){
     e.preventDefault();
     
     
-    if(nameInput.value ==='' || emailInput.value === '' || msgField.value ===''){
+    if(nameInput.value ==='' || emailInput.value === '' ){
 
+        msg.classList.add('error')
+        msg.innerHTML = 'please enter all field';
+        setTimeout(()=> msg.remove(), 3000);
+    }
+    if(emailInput.value ==='' || msgField.value ===''){
         msg.classList.add('error')
         msg.innerHTML = 'please enter all field';
         setTimeout(()=> msg.remove(), 3000);
     }
     else{
     msg.classList.add('success')
-    msg.innerHTML = 'successfull sent a message to Mr Emanikor';
+    msg.innerHTML = 'successfull sent a message';
     setTimeout(()=> msg.remove(), 3000);
     }
 }
